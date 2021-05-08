080b8727f15b97e7c6cf7be6d0e785358bdcf51c
siddharth-stn <66431369+siddharth-stn@users.noreply.github.com>
2021-05-08 18:45:28 +0530

    added wallpaper image from firebase storage, working on timer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
 import FrontEnd from "./FrontEnd";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBMB1b2vAXpWaVNTiQpZYhvrZ4cO09Yens",
-  authDomain: "waldo-sid.firebaseapp.com",
-  projectId: "waldo-sid",
-  storageBucket: "waldo-sid.appspot.com",
-  messagingSenderId: "167494571860",
-  appId: "1:167494571860:web:395d19f0a18d0a49105ac1",
-  measurementId: "G-MR7WT9LVY1",
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
-
 function App() {
-  return (
-    <FrontEnd />
-  );
+  return <FrontEnd />;
 }
 
 export default App;
--- src/FrontEnd.js
+++ src/FrontEnd.js
@@ -1,8 +1,31 @@
-import { useState } from "react";
-import wallpaper from "./static/findWaldo.jpg";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/storage";
+import { useEffect, useState } from "react";
+//import wallpaper from "./static/findWaldo.jpg";
 import waldoCropped from "./static/waldoCropped.jpg";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBMB1b2vAXpWaVNTiQpZYhvrZ4cO09Yens",
+  authDomain: "waldo-sid.firebaseapp.com",
+  projectId: "waldo-sid",
+  storageBucket: "waldo-sid.appspot.com",
+  messagingSenderId: "167494571860",
+  appId: "1:167494571860:web:395d19f0a18d0a49105ac1",
+  measurementId: "G-MR7WT9LVY1",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+//const db = firebase.firestore();
+const storage = firebase.storage();
+const storageRef = storage.ref();
+const imageRef = storageRef.child("findWaldo.jpg");
+
 function FrontEnd() {
+  const [url, setUrl] = useState("");
+
   const [XPercent, setXPercent] = useState(0);
   const [YPercent, setYPercent] = useState(0);
 
@@ -27,9 +50,20 @@ function FrontEnd() {
     console.log(XPercent, " and ", YPercent);
   }
 
-  function handleBtnClick(event) {
+  async function handleBtnClick(event) {
+    setUrl(await imageRef.getDownloadURL());
     event.target.classList.add("hidden");
     document.getElementsByClassName("imgDiv")[0].classList.remove("hidden");
+  }
+
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    let timerID = setInterval(() => clock(), 1000);
+  });
+
+  function clock() {
+    setTime(new Date());
   }
 
   return (
@@ -39,7 +73,9 @@ function FrontEnd() {
         <h2 className="title">
           <span>Where's</span> <span className="badge bg-danger">Waldo?</span>
         </h2>
+        <div class="timerDiv">{time.toLocaleTimeString()}</div>
       </div>
+
       <button
         type="button"
         onClick={handleBtnClick}
@@ -50,7 +86,7 @@ function FrontEnd() {
       <div className="imgDiv hidden">
         <img
           className="wallpaperImage"
-          src={wallpaper}
+          src={url}
           onClick={clickPhoto}
           alt="Waldo wallpaper"
         />
