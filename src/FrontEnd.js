4d631a5a22a3c974053d02f44ae7e800004bbcfc
siddharth-stn <66431369+siddharth-stn@users.noreply.github.com>
2021-05-13 20:51:21 +0530

    Added code to display wrong choice div

--- src/FrontEnd.js
+++ src/FrontEnd.js
@@ -7,6 +7,7 @@ import waldoCropped from "./static/waldoCropped.jpg";
 import odlaw from "./static/odlaw.png";
 import waldo from "./static/waldo.png";
 import wizard from "./static/wizard.png";
+import check from "./static/checkMark.png";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -19,16 +20,15 @@ const firebaseConfig = {
   measurementId: "G-MR7WT9LVY1",
 };
 
-// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-// const db = firebase.firestore();
+const db = firebase.firestore();
 // const storage = firebase.storage();
 // const storageRef = storage.ref();
 // const imageRef = storageRef.child("findWaldo.jpg");
 const imageRef = ""; // *** Delete this line after refactoring the whole code
 
 function getTimer() {
-  console.log("I was Run");
   const timeInSec = Math.floor(new Date().getTime() / 1000);
   return timeInSec;
 }
@@ -63,31 +63,117 @@ function calcMin(timeInSec) {
   return quotient;
 }
 
+const foundArray = [];
+const liItemsArray = ["Waldo", "Odlaw", "Wizard"];
+
 function FrontEnd() {
+  // *** React Component Starts here --------->
   const [url, setUrl] = useState("");
 
   const [XPercent, setXPercent] = useState(0);
   const [YPercent, setYPercent] = useState(0);
 
   const body = document.getElementsByTagName("body")[0];
-  body.addEventListener("click", (event) => {
-    if (event.target.className !== "wallpaperImage") {
+  body.addEventListener("click", function getBodyClick(event) {
+    if (
+      event.target.className !== "wallpaperImage" &&
+      event.target.tagName !== "LI"
+    ) {
       document.getElementsByClassName("dropdownDiv")[0].classList.add("hidden");
-      console.log(event.target.className);
+      console.log(event.target.tagName);
     }
+    return body.removeEventListener("click", getBodyClick);
   });
 
+  const dropdownDiv = document.getElementsByClassName("dropdownDiv")[0];
+
+  useEffect(() => {
+    if (dropdownDiv) {
+      dropdownDiv.addEventListener("click", function getDropClickEvent(event) {
+        const name = event.target.innerText;
+        foundArray.forEach((item) => {
+          if (item === name) {
+            console.log(`I found ${name}`);
+          }
+        });
+        const imagesDbRef = db.collection("images");
+        const queryRef = imagesDbRef.where("name", "==", name);
+        queryRef
+          .get()
+          .then((snapshot) => {
+            snapshot.forEach((doc) => {
+              const xLocationInDb = [];
+              const yLocationInDb = [];
+              xLocationInDb.push(doc.data().location.x[0]);
+              xLocationInDb.push(doc.data().location.x[1]);
+              yLocationInDb.push(doc.data().location.y[0]);
+              yLocationInDb.push(doc.data().location.y[1]);
+              if (
+                Number(XPercent) >= Number(xLocationInDb[0]) &&
+                Number(XPercent) <= Number(xLocationInDb[1])
+              ) {
+                if (
+                  Number(YPercent) >= Number(yLocationInDb[0]) &&
+                  Number(YPercent) <= Number(yLocationInDb[1])
+                ) {
+                  foundArray.push(name);
+                  foundArray.forEach((item) => {
+                    switch (item) {
+                      case "Waldo":
+                        document
+                          .getElementById("waldoTickDiv")
+                          .classList.remove("hidden");
+                        break;
+
+                      case "Odlaw":
+                        document
+                          .getElementById("odlawTickDiv")
+                          .classList.remove("hidden");
+                        break;
+
+                      case "Wizard":
+                        document
+                          .getElementById("wizardTickDiv")
+                          .classList.remove("hidden");
+                        break;
+                    }
+                  });
+                  liItemsArray.forEach((item, index) => {
+                    if (item === name) {
+                      liItemsArray.splice(index, 1);
+                    }
+                  });
+                }
+              } else {
+                console.log(`That's not ${name}`);
+              }
+            });
+          })
+          .catch((error) => {
+            console.error("Can not return document: ", error);
+          });
+        dropdownDiv.classList.add("hidden");
+        dropdownDiv.removeEventListener("click", getDropClickEvent);
+      });
+    }
+  }, [XPercent, YPercent]);
+
   function clickPhoto(event) {
-    setXPercent(
-      Math.floor((event.nativeEvent.offsetX / event.nativeEvent.target.offsetWidth) * 100
-    ));
-    setYPercent(
-      Math.floor((event.nativeEvent.offsetY / event.nativeEvent.target.offsetHeight) * 100
-    ));
+    let xper = Math.floor(
+      (event.nativeEvent.offsetX / event.nativeEvent.target.offsetWidth) * 100
+    );
+
+    let yper = Math.floor(
+      (event.nativeEvent.offsetY / event.nativeEvent.target.offsetHeight) * 100
+    );
+
+    setXPercent(xper);
+    setYPercent(yper);
+
     document
       .getElementsByClassName("dropdownDiv")[0]
       .classList.remove("hidden");
-    console.log(XPercent, " and ", YPercent);
+    //console.log(xper, " and ", yper);
   }
 
   const [min, setMin] = useState("00");
@@ -95,7 +181,7 @@ function FrontEnd() {
   const [startClock, setStartClock] = useState(false);
 
   function clock() {
-    if (startClock != false) {
+    if (startClock !== false) {
       let timeDiff = getTimer() - startClock;
       let sec = calcSec(timeDiff);
       let min = calcMin(timeDiff);
@@ -105,7 +191,7 @@ function FrontEnd() {
   }
 
   useEffect(() => {
-    if (startClock != false) {
+    if (startClock !== false) {
       let timerID = setInterval(() => clock(), 1000);
       return function cleanup() {
         clearInterval(timerID);
@@ -118,6 +204,10 @@ function FrontEnd() {
     event.target.classList.add("hidden");
     document.getElementsByClassName("imgDiv")[0].classList.remove("hidden");
     setStartClock(getTimer());
+  }
+
+  function showLiItems() {
+    return liItemsArray.map((item) => <li>{item}</li>);
   }
 
   return (
@@ -135,16 +225,25 @@ function FrontEnd() {
       <div className="mainDiv">
         <div className="sideDiv">
           <div className="waldoFaceDiv">
+            <div id="waldoTickDiv" className="tickWaldo hidden">
+              <img src={check} />
+            </div>
             <p>Waldo</p>
-            <img src={waldo} />
+            <img src={waldo} alt="waldo face" />
           </div>
           <div className="odlawFaceDiv">
+            <div id="odlawTickDiv" className="tickOdlaw hidden">
+              <img src={check} />
+            </div>
             <p>Odlaw</p>
-            <img src={odlaw} />
+            <img src={odlaw} alt="odlaw face" />
           </div>
           <div className="wizardFaceDiv">
+            <div id="wizardTickDiv" className="tickWizard hidden">
+              <img src={check} />
+            </div>
             <p>Wizard</p>
-            <img src={wizard} />
+            <img src={wizard} alt="wizard face" />
           </div>
         </div>
         <button
@@ -155,6 +254,9 @@ function FrontEnd() {
           Find Me!
         </button>
         <div className="imgDiv hidden">
+          <div id="indicatorDiv" className="hidden">
+            Wrong Choice!!!!
+          </div>
           <img
             id="wallImg"
             className="wallpaperImage"
@@ -166,11 +268,7 @@ function FrontEnd() {
             className="dropdownDiv hidden"
             style={{ left: XPercent + "%", top: YPercent + "%" }}
           >
-            <ul>
-              <li>Waldo</li>
-              <li>Odlaw</li>
-              <li>Wizard</li>
-            </ul>
+            <ul>{showLiItems()}</ul>
           </div>
         </div>
       </div>
